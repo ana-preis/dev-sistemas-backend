ef483e30168ffd75a9f119720fa4312b07f16418
ana-preis <anarps_@hotmail.com>
2023-02-23 21:41:55 -0300

    add trycatch to notas

--- atv04-notas/src/Main.java
+++ atv04-notas/src/Main.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Quantas notas vai colocar?");
-        int qtdNotas = in.nextInt();
-        System.out.println("--------------------");
-
-        float somaNotas = 0;
-        for (int i = 0; i < qtdNotas; i++) {
-            System.out.println("Digite a " + (i+1) + "ª nota:");
-            float nota = in.nextFloat();
-            somaNotas += nota;
-        }
-        float media = somaNotas/qtdNotas;
-        System.out.println("A média das notas é: " + media);
-        if (media > 7) {
-            System.out.println("Aluno aprovado!");
-        } else {
-            System.out.println("Aluno reprovado!");
+        try{
+            Scanner in = new Scanner(System.in);
+            System.out.println("Quantas notas vai colocar?");
+            int qtdNotas = in.nextInt();
+            System.out.println("--------------------");
+            float somaNotas = 0;
+            for (int i = 0; i < qtdNotas; i++) {
+                System.out.println("Digite a " + (i+1) + "ª nota:");
+                float nota = in.nextFloat();
+                somaNotas += nota;
+            }
+            float media = somaNotas/qtdNotas;
+            System.out.println("A média das notas é: " + media);
+            if (media > 7) {
+                System.out.println("Aluno aprovado!");
+            } else {
+                System.out.println("Aluno reprovado!");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Valor inválido!!");
+        } catch (RuntimeException er) {
+            System.out.println("Erro ao rodar o script!");
         }
     }
 }
